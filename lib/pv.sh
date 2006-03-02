6bbb57e5abe01602221848fbd5538cf6a5aa56d5
Didier Link <didier@famille-link.fr>
2006-03-02 22:26:14 +0000

    patch from gpl ghostscript 8.50 (7/16) : lib

--- lib/pv.sh
+++ lib/pv.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -f
-# $Id: pv.sh,v 1.2 2004/02/14 22:20:04 atai Exp $
+# $Id: pv.sh,v 1.3 2006/03/02 22:26:14 Arabidopsis Exp $
 #
 # pv - preview a specified page of a dvi file in a Ghostscript window
 # usage: pv page file
@@ -25,6 +25,11 @@ fi
 # are held on the system."  So we have deleted this line, and also removed
 # the -D switch from the call of dvips below.
 #
+
+# This definition is changed on install to match the
+# executable name set in the makefile
+GS_EXECUTABLE=gs
+
 TEMPDIR=.
 PAGE=$1
 shift
@@ -33,5 +38,5 @@ shift
 trap "rm -rf $TEMPDIR/$FILE.$$.pv" 0 1 2 15
 #dvips -D$RESOLUTION -p $PAGE -n 1 $FILE $* -o $FILE.$$.pv
 dvips -p $PAGE -n 1 $FILE $* -o $FILE.$$.pv
-gs $FILE.$$.pv
+$GS_EXECUTABLE $FILE.$$.pv
 exit 0
